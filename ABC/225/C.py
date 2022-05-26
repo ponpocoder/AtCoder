902aef0786a4e8f005cf46f5911c5dd93e3a399d
ponpocoder <naotaku578@gmail.com>
2022-05-26 19:23:50 +0900

    精進

--- ABC/225/C.py
+++ ABC/225/C.py
@@ -2,6 +2,12 @@ import sys
 
 n, m = map(int, input().split())
 b = [list(map(int, input().split())) for _ in range(n)]
+row = (b[0][0] - 1) // 7
+col = (b[0][0] - 1) % 7
+
+if col + len(b[0]) >= 8:
+    print("No")
+    sys.exit()
 
 for i in range(n):
     if i < n - 1 and b[i+1][0] != b[i][0] + 7:
